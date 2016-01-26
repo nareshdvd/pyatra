29f96d06ba2f151e54901da2a15d7fdad004ea06
nd-rordev <naresh.dwivedi@traveltriangle.com>
2016-01-26 19:12:09 +0530

    rendered project git setup

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from accounts.models import UsageToken
-# Register your models here.
-
-class UsageTokenAdmin(admin.ModelAdmin):
-    exclude = ('token',)
-admin.site.register(UsageToken, UsageTokenAdmin)
-
